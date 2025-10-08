ccfc4cfc3d5e5bfd91f6b7cee119dc0085481c9e
Jota <jsaldana999@gmail.com>
2025-10-07 20:51:29 -0500

    feat: restructure application with new routing and components for login, games, and companies

--- src/Empresa.tsx
+++
@@ -1,9 +0,0 @@
-function Empresa() {
-  return (
-    <>
-      <h1>Empresa</h1>
-    </>
-  );
-}
-
-export default Empresa;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./About.tsx";
-import Empresa from "./Empresa.tsx";
+import Login from "./Login.tsx";
+import Games from "./Games.tsx";
+import Companies from "./Companies.tsx";
+import Error from "./Error.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route index element={<App />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/empresa" element={<Empresa />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/games" element={<Games />} />
+      <Route path="/companies" element={<Companies />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   </BrowserRouter>
 );
